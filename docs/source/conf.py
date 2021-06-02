7ead2dad6e20eac47a97228acd48952d9f35197e
zhanzghiyong0410 <2556703570@qq.com>
2021-06-02 16:36:04 +0800

    update js and css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,12 @@ html_static_path = ['_static']
 	
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+#公司自定义js和css
+html_js_files = [
+    'https://ezblock.cc/readDocFile/topHead.js',
+]
+html_css_files = [
+    'https://ezblock.cc/readDocFile/topHead.css',
+]
+
