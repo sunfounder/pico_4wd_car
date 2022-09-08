01b496e84351f287960c9fd384c24bf3cd346e54
Lovmong <1967471283@qq.com>
2022-09-08 18:34:00 +0800

    fix serial data error and long delay, add led status dispaly

--- esp8266-uart-wsserver-v1.0.1/esp8266-uart-wsserver-v1.0.1.ino
+++ esp8266-uart-wsserver-v1.0.1/esp8266-uart-wsserver-v1.0.1.ino
@@ -1,11 +1,30 @@
-#include <WebSocketsServer.h>
+ #include <WebSocketsServer.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 
 // for production, uncomment this line
 // #define DEBUG
 
+#define BUILTIN_LED1 2 // BUILTIN_LED GPIO2
+#define LED_OFF 0
+#define LED_ON 1
+#define LED_SLOW_BLINK 2
+#define LED_FAST_BLINK 3
+#define SLOW_BLINK_DELAY 500 // uint ms
+#define FAST_BLINK_DELAY 100
+
+#define LED_STATUS_DISCONNECTED()  builtin_led_slow_blink()
+#define LED_STATUS_CONNECTED()  builtin_led_on()
+#define LED_STATUS_ERROOR()  builtin_led_fast_blink()
+
+bool builtin_led_enable = true; // Whether to enable LED
+double led_startMillis = 0;  // LED state start time
+uint8_t builtin_led_status = LED_OFF;
+bool led_status_flag = false;
+
 #define SERIAL_TIMEOUT 100 // 100ms
+String rxBuf = "";
+bool rx_complete = false;
 
 // Mode
 #define NONE 0
@@ -20,23 +39,26 @@ int mode = NONE;
 
 bool isConnected = false;
 String ip = "";
-String rxBuf = "";
 uint8_t client_num = 0;
+double time_count = 0;
 
 WebSocketsServer webSocket = WebSocketsServer(8765);
 
 bool wifiClient();
 bool wifiAP();
 bool connectWiFi();
-String serialRead();
 void handleSet(String cmd);
 void handleGet(String cmd);
 String intToString(uint8_t * value, size_t length);
 void onWebSocketEvent(uint8_t cn, WStype_t type, uint8_t * payload, size_t length);
+void builtin_led_init();
+void serialRead();
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(SERIAL_TIMEOUT);
+  builtin_led_init();
+  LED_STATUS_DISCONNECTED();
   #ifdef DEBUG
   Serial.println("[DEBUG] Start!");
   #endif
@@ -44,12 +66,21 @@ void setup() {
 }
 
 void loop() {
-  rxBuf = serialRead();
-  if (rxBuf.length() > 0) {
+  // time_count = millis();
+  builtin_led_status_handler();
+  // Serial.print("led_ut:");Serial.println(millis() - time_count);
+
+  // time_count = millis();
+  serialRead();
+  // Serial.print("rxS_ut:");Serial.println(millis() - time_count);
+
+  // time_count = millis();
+  if (rx_complete == true) {
+    // Serial.print("rx:");Serial.println(rxBuf);
+    // delay(100);
     #ifdef DEBUG
     Serial.print("[DEBUG] RX Receive: ");Serial.println(rxBuf);
     #endif
-    delay(10);
     if (rxBuf.substring(0, 4) == "SET+"){
       handleSet(rxBuf.substring(4));
     } else if (rxBuf.substring(0, 3) == "WS+"){
@@ -59,10 +90,18 @@ void loop() {
       #endif
       webSocket.sendTXT(client_num, out);
     }
+    rxBuf = "";
+    rx_complete = false;
+
   }
+  // Serial.print("rxH_ut:");Serial.println(millis() - time_count);
+
+  // time_count = millis();
   if (isConnected){
     webSocket.loop();
   }
+  // Serial.print("wsL_ut:");Serial.println(millis() - time_count);
+
   delay(10);
 }
 
@@ -138,21 +177,17 @@ bool connectWiFi(){
   return true;
 }
 
-String serialRead() {
-  String buf = "";
+void serialRead() {
   char inChar;
-  int temp;
-  unsigned long timeoutStart = millis();
-  while (Serial.available() && millis() - timeoutStart < SERIAL_TIMEOUT) {
-    temp = Serial.read();
-    inChar = (char)temp;
+  while (Serial.available()) {
+    inChar = (char)Serial.read();
     if (inChar == '\n') {
+      rx_complete = true;
       break;
     } else if((int)inChar != 255){
-      buf += inChar;
+      rxBuf += inChar;
     }
   }
-  return buf;
 }
 
 void handleSet(String cmd){
@@ -182,6 +217,17 @@ void handleSet(String cmd){
       Serial.println("AP");
     } else if (mode == STA) {
       Serial.println("STA");
+    }
+    #endif
+    Serial.println("[OK]");
+  }else if (cmd.substring(0, 3) == "LED"){
+    builtin_led_enable = cmd.substring(3).toInt();
+    #ifdef DEBUG
+    Serial.print("[DEBUG] Set LED: ");
+    if (builtin_led_enable == 0){
+      Serial.println("disable");
+    } else if (builtin_led_enable == 1) {
+      Serial.println("enable");
     }
     #endif
     Serial.println("[OK]");
@@ -243,6 +289,7 @@ void onWebSocketEvent(uint8_t cn, WStype_t type, uint8_t * payload, size_t lengt
       Serial.println("[DEBUG] [WS] Disconnected!");
       #endif
       IPAddress remoteIp = webSocket.remoteIP(client_num);
+      LED_STATUS_DISCONNECTED();
       Serial.print("[DISCONNECTED] ");Serial.println(remoteIp.toString());
       break;
     }
@@ -253,6 +300,7 @@ void onWebSocketEvent(uint8_t cn, WStype_t type, uint8_t * payload, size_t lengt
       Serial.print("[DEBUG] [WS] Connection from ");
       Serial.println(remoteIp.toString());
       #endif
+      LED_STATUS_CONNECTED();
       Serial.print("[CONNECTED] ");Serial.println(remoteIp.toString());
       // out = serialReadBlock();
       // webSocket.sendTXT(client_num, out);
@@ -282,6 +330,7 @@ void onWebSocketEvent(uint8_t cn, WStype_t type, uint8_t * payload, size_t lengt
       break;
     }
     case WStype_ERROR: {
+      LED_STATUS_ERROOR();
       #ifdef DEBUG
       Serial.println("[DEBUG] [WS] WStype_ERROR");
       #endif
@@ -330,4 +379,54 @@ void onWebSocketEvent(uint8_t cn, WStype_t type, uint8_t * payload, size_t lengt
       break;
     }
   }
+}
+
+void builtin_led_init() {
+  pinMode(BUILTIN_LED1, OUTPUT);  // Set LED pin as output
+  digitalWrite(BUILTIN_LED1, HIGH);  // 1:turn off LED
+}
+
+void builtin_led_off() {
+  builtin_led_status = LED_OFF;
+}
+
+void builtin_led_on() {
+  builtin_led_status = LED_ON;
+}
+
+void builtin_led_slow_blink() {
+  builtin_led_status = LED_SLOW_BLINK;
+}
+
+void builtin_led_fast_blink() {
+  builtin_led_status = LED_FAST_BLINK;
+}
+
+void builtin_led_status_handler() {
+  if (builtin_led_enable){
+    switch (builtin_led_status) {
+        case LED_OFF:
+          digitalWrite(BUILTIN_LED1, HIGH);
+          break;
+        case LED_ON:
+          digitalWrite(BUILTIN_LED1, LOW);
+          break;
+        case LED_SLOW_BLINK:
+          if (millis() - led_startMillis > SLOW_BLINK_DELAY){
+            led_startMillis = millis();
+            led_status_flag = !led_status_flag;
+            digitalWrite(BUILTIN_LED1, led_status_flag); // slow blink
+          }
+          break;
+        case LED_FAST_BLINK:
+          if (millis() - led_startMillis > FAST_BLINK_DELAY){
+            led_startMillis = millis();
+            led_status_flag = !led_status_flag;
+            digitalWrite(BUILTIN_LED1, led_status_flag); // fast blink
+          }
+      }
+  }else{
+    digitalWrite(BUILTIN_LED1, HIGH);
+  }
+  
 }
