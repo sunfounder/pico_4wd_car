3f411c17cd579b98b44c959a536551805ab2e8fa
Lovmong <1967471283@qq.com>
2023-02-03 11:10:06 +0800

    change follow_hand method

--- examples/follow_hand.py
+++ examples/follow_hand.py
@@ -1,39 +1,58 @@
 import pico_4wd as car
 
-car.RADAR_REFERENCE = 25
+car.RADAR_REFERENCE = 20
 car.RADAR_MAX_ANGLE = 45
 car.RADAR_MIN_ANGLE = -45
 car.RADAR_STEP_ANGLE = 10
-MOTOR_FORWARD_POWER = 40
-MOTOR_TURNING_POWER = 40
+MOTOR_FORWARD_POWER = 20
+MOTOR_TURNING_POWER = 15
+
+
+def get_dir(radar_data, split_str="0"):
+    # get scan status of 0, 1
+    radar_data = [str(i) for i in radar_data]
+    radar_data = "".join(radar_data)
+
+    # Split 0, leaves the free path
+    paths = radar_data.split(split_str)
+
+    # Calculate where is the widest
+    max_paths = max(paths)
+    if split_str == "0" and len(max_paths) < 4:
+        return "left"
+    elif split_str == "1" and len(max_paths) < 2:
+        return "stop"
+
+    # Calculate the direction of the widest 
+    pos = radar_data.index(max_paths)
+    pos += (len(max_paths) - 1) / 2 
+    delta = len(radar_data) / 3
+    if pos < delta:
+        return "left"
+    elif pos > 2 * delta:
+        return "right"
+    else:
+        return "forward"
+
 
 def main():
     while True:
-        radar_data = car.radar_scan()
+        _, _,radar_data = car.radar_scan()
+
+        # If radar data return a int, means scan not finished, and the int is current angle status
         if isinstance(radar_data, int):
             continue
-        radar_data = [str(i) for i in radar_data]
-        radar_data = "".join(radar_data)
-        paths = radar_data.split("1")
-        length_list = []
-        for path in paths:
-            length_list.append(len(path))
-        if max(length_list) == 0:
-            car.move("stop") 
+
+        direction = get_dir(radar_data, split_str='1')
+
+        if direction == "left":
+            car.move("left", MOTOR_TURNING_POWER)
+        elif direction == "right":
+            car.move("right", MOTOR_TURNING_POWER)
+        elif direction == "forward":
+            car.move("forward", MOTOR_FORWARD_POWER)
         else:
-            i = length_list.index(max(length_list))
-            pos = radar_data.index(paths[i])
-            pos += (len(paths[i]) - 1) / 2
-            delta = len(radar_data) / 3
-            if pos < delta:
-                car.move("right", MOTOR_TURNING_POWER)
-            elif pos > 2 * delta:
-                car.move("left", MOTOR_TURNING_POWER)
-            else:
-                #if radar_data[int(len(radar_data)/2-1)] == "0":
-                #    car.move("stop")
-                #else:
-                car.move("forward", MOTOR_FORWARD_POWER)
+            car.move("stop") 
 
 try:
     main()
